Fixing monotonicity constraint reading, adding normalization check for feature range initialization

src/cefeature/feature_sampler.py
@@ -44,20 +44,20 @@ class ChoiceSampler(ICEFeatureSampler):
         return self._choice_sample()
 
 class MonotonicSampler(ICEFeatureSampler):
-    def __init__(self, feature_name, feature_range, sign: int):
+    def __init__(self, feature_name, feature_range, sign):
         super().__init__(feature_name, feature_range)
         self._set_sign(sign)
 
     def _set_sign(self, sign):
-        assert sign in [-1, 1], "Sign must be either -1 or 1"
+        assert sign in ["increasing", "decreasing"], "Sign must be either increasing or decreasing"
         self.sign = sign
 
     def _increasing_sample(self, feature_value):
-        assert self.sign == 1, "Sign must be 1 for increasing sample"
+        assert self.sign == "increasing", "Config must be increasing for increasing sample"
         return np.random.uniform(feature_value, self.feature_range[1])
 
     def _decreasing_sample(self, feature_value):
-        assert self.sign == -1, "Sign must be -1 for decreasing sample"
+        assert self.sign == "decreasing", "Config must be decreasing for decreasing sample"
         return np.random.uniform(self.feature_range[0], feature_value)
 
     def sample(self, instance):

src/transformer.py
@@ -23,10 +23,11 @@ class FeatureTransformer(object):
             self.normalized_range = self.get_normalized_range(config, dataset)
 
     def calculate_statistics(self, dataset):
-        return dataset.data[self.feature_name].agg([min, max, np.mean, np.std, np.median]).to_dict()
+        return dataset.data[self.feature_name].agg([min, max, np.mean, np.std, np.median])
 
     def get_from_dataset(self, dataset):
-        return dataset.data[self.feature_name].agg([min, max]).to_dict()
+        aggregated_values = dataset.data[self.feature_name].agg([min, max])
+        return [aggregated_values['min'], aggregated_values['max']]
     
     def get_normalized_range(self, config, dataset):
         if config["continuous_features"]["normalization"] == "minmax":
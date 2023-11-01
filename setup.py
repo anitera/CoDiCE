090d73470bbd92889d48fc27fdd2fcd4b1b752ca
anitera <atira95@gmail.com>
2023-11-01 15:53:50 +0200

    adding more packages

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ setup(
         "pandas==2.1.1",
         "scikit-learn==1.3.1",
         "scipy==1.11.3",
-        "pydiffmap==0.2.0.1"
-    ],
+        "pydiffmap==0.2.0.1",
+        "Pillow==10.0.1".
+        "PyYAML==6.0.1",
+    ]
     extras_require={
         "dev": [
             "pytest>=3.4.3",
-            "ipython>=7.2.0",
+            "ipykernel>=6.0.3",
         ],
         "sklearn": ["scikit-learn>=0.24.1"],
         "tensorflow": ["tensorflow>=2.4.1"],

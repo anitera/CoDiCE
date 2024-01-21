c1b0d43427db26028f1b3312083b465d526ddc44
anitera <atira95@gmail.com>
2024-01-21 16:34:50 +0200

    ipykernel version update, pandas version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trustce",
-    version="0.2.7",
+    version="0.2.8",
     packages=find_packages(),
     package_data={
         "trustce": ["input_instance/*.json", "config/*.yaml", "config/*.json", "examples_notebooks_demos/*.ipynb", "models/*.pkl", "models/*.pth"]
@@ -10,19 +10,18 @@ setup(
     data_files=[("", ["LICENSE", 'README.md'])],
     install_requires=[
         "numpy==1.26.1",
-        "pandas==2.1.1",
+        "pandas==1.5.3",
         "scikit-learn==1.3.1",
         "scipy==1.11.3",
         "pydiffmap==0.2.0.1",
         "Pillow==10.0.1",
         "PyYAML==6.0.1",
         "requests==2.31.0",
-        "ipykernel>=6.0.3",
+        "ipykernel>=5.5.6",
     ],
     extras_require={
         "dev": [
-            "pytest>=3.4.3",
-            "ipykernel>=6.0.3",
+            "pytest>=7.4.4"
         ],
         "sklearn": ["scikit-learn>=0.24.1"],
         "tensorflow": ["tensorflow>=2.14.1"],

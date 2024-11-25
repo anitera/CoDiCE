33a29320383daa3d0b9b62233f57b406585a045c
anitera <atira95@gmail.com>
2024-11-25 13:34:43 +0200

    added support for json configuration format in addition to yml

--- codice/config.py
+++ codice/config.py
@@ -1,4 +1,6 @@
 import yaml
+import json
+import os
 
 class Config:
     def __init__(self, config_path):
@@ -6,8 +8,16 @@ class Config:
         self.config = self.load_config()
 
     def load_config(self):
+        # Determine the file extension
+        file_extension = os.path.splitext(self.config_path)[1]
+        
         with open(self.config_path, 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
+            if file_extension in ['.yaml', '.yml']:
+                config = yaml.load(f, Loader=yaml.FullLoader)
+            elif file_extension in ['.json']:
+                config = json.load(f)
+            else:
+                raise ValueError("Unsupported file type: {}".format(file_extension))
         return config
 
     def get_config(self):
@@ -23,5 +33,11 @@ class Config:
         self.config[key] = value
 
     def save_config(self):
+        file_extension = os.path.splitext(self.config_path)[1]
         with open(self.config_path, 'w') as f:
-            yaml.dump(self.config, f)
+            if file_extension in ['.yaml', '.yml']:
+                yaml.dump(self.config, f)
+            elif file_extension in ['.json']:
+                json.dump(self.config, f)
+            else:
+                raise ValueError("Unsupported file type: {}".format(file_extension))
